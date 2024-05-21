4a6e525c42d3209a990e37acafdddcf3757670e4
Rogerdev <andreshackingdev@andreshackingdev.com>
2024-05-21 15:15:52 -0500

    fix

--- src2/front/pages/shop/Shop.tsx
+++
@@ -1,9 +0,0 @@
-import {Text, View} from 'react-native';
-
-export const Shop = () => {
-  return (
-    <View>
-      <Text>Shop</Text>
-    </View>
-  );
-};
--- src2/types/env.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '@env' {
-  export const STAGE: string;
-  export const API_URL: string;
-}
